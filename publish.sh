5af506b1c5188b81cf2cf1259814e3209204c282
Dmitriy <d.vishnevskiy@outlook.com>
2020-01-23 18:20:55 +0300

    Publish commit

--- publish.sh
+++ publish.sh
@@ -1,6 +1,6 @@
 ((num1=$(printenv ACTIVE_CHECKER)+1))
 export ACTIVE_CHECKER=$num1
-gem build active_checker.gemspec --output=active_checker.gem
+gem build active_checker.gemspec
 git add .
 git commit -m "Publish commit"
 git push origin master
